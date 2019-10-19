32118982d270a1fd7f0add47fecd394289d65adf
cummi <cummingsdan87@gmail.com>
2019-10-19 11:27:51 -0400

    Default Scroll activity

--- app/src/main/java/com/marksimonyi/android/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/marksimonyi/android/cst2335finalproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.marksimonyi.android.cst2335finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button newsButton = findViewById(R.id.news_button);
+        newsButton.setOnClickListener(btn -> {
+            Intent goToNewsMain = new Intent(MainActivity.this, NewsMain.class);
+            startActivity(goToNewsMain);
+        });
     }
 }
