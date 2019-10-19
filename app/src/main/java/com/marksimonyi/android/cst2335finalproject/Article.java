133f8342b333bfe55b2990fc57e4bb6f06433cf4
cummi <cummingsdan87@gmail.com>
2019-10-19 12:23:55 -0400

    applyed Article

---
+++ app/src/main/java/com/marksimonyi/android/cst2335finalproject/Article.java
@@ -0,0 +1,25 @@
+package com.marksimonyi.android.cst2335finalproject;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.webkit.WebView;
+
+public class Article extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        WebView website = (WebView) findViewById(R.id.webview);
+
+        website.loadUrl("www.example.com");
+    }
+}
