9a6a126d7e45da2f8ab098cc785b9e57388aac78
MarkSimonyi <simo0100@algonquinlive.com>
2019-10-28 18:07:25 -0400

    Backup; working on checkpoint 1.

--- app/src/main/java/com/marksimonyi/android/cst2335finalproject/RecipeActivity.java
+++ app/src/main/java/com/marksimonyi/android/cst2335finalproject/RecipeActivity.java
@@ -1,4 +1,18 @@
 package com.marksimonyi.android.cst2335finalproject;
 
-public class RecipeActivity {
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    public static final String ACTIVITY_NAME = "RECIPE_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e(ACTIVITY_NAME, "In function: onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_activity);
+
+    }
 }
