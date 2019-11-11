a93c8df95683d622baf28ae6496dbeda65222d44
cummi <cummingsdan87@gmail.com>
2019-11-11 15:22:15 -0500

    applyed Article

--- app/src/main/java/com/marksimonyi/android/cst2335finalproject/NewsMain.java
+++ app/src/main/java/com/marksimonyi/android/cst2335finalproject/NewsMain.java
@@ -16,8 +16,6 @@ public class NewsMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 }
