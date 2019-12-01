2aa19216b826bb2dddb4c2de5a2e499731ff0746
port0227 <port0227@algonquinlive.com>
2019-12-01 18:07:00 -0500

    Foreign exchange milestone 1

---
+++ app/src/main/java/com/marksimonyi/android/cst2335finalproject/NewsMain.java
@@ -0,0 +1,16 @@
+package com.marksimonyi.android.cst2335finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_main);
+
+
+    }
+}
